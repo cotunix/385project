bdb0068997a4da8987c025f0778b578b26acaef2
Kyle Richardson <richarkc@miamioh.edu>
2014-11-13 23:51:50 -0500

    Prototype complete

--- src/dbproject/GUI.java
+++ src/dbproject/GUI.java
@@ -1,11 +1,5 @@
 package dbproject;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.*;
@@ -15,27 +9,33 @@ import javax.swing.DefaultListModel;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 
-
 public class GUI extends javax.swing.JFrame {
+
+	private static final long serialVersionUID = -270004264217043148L;
 
 	public GUI() {
 		initComponents();
 	}
 
-
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	// <editor-fold defaultstate="collapsed" desc="Generated Code">
 	private void initComponents() {
 
 		jScrollPane1 = new javax.swing.JScrollPane();
 		jList1 = new javax.swing.JList<String>();
 		jButton1 = new javax.swing.JButton();
+		jButton2 = new javax.swing.JButton();
 		jComboBox1 = new javax.swing.JComboBox<String>();
 		jComboBox2 = new javax.swing.JComboBox<String>();
-		jComboBox3 = new javax.swing.JComboBox();
-		jComboBox4 = new javax.swing.JComboBox();
-		jComboBox5 = new javax.swing.JComboBox();
+		jComboBox3 = new javax.swing.JComboBox<String>();
+		jComboBox4 = new javax.swing.JComboBox<String>();
+		jComboBox5 = new javax.swing.JComboBox<String>();
+		label1 = new java.awt.Label();
 
+		label1.setText("Routes:");
+
+		setTitle("BCRTA Route App");
+		setResizable(false);
 		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 		listModel = new DefaultListModel<String>();
 		jList1.setModel(listModel);
@@ -47,6 +47,7 @@ public class GUI extends javax.swing.JFrame {
 				jButton1ActionPerformed(evt);
 			}
 		});
+		jButton2.setText("Admin Login");
 
 		jList1.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent evt) {
@@ -67,7 +68,8 @@ public class GUI extends javax.swing.JFrame {
 				.toArray()));
 		jComboBox3.setModel(new javax.swing.DefaultComboBoxModel(days));
 		jComboBox4.setModel(new javax.swing.DefaultComboBoxModel(times));
-		jComboBox5.setModel(new javax.swing.DefaultComboBoxModel(new String[] {"A.M", "P.M"}));
+		jComboBox5.setModel(new javax.swing.DefaultComboBoxModel(new String[] {
+				"A.M", "P.M" }));
 
 		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
 				getContentPane());
@@ -75,20 +77,11 @@ public class GUI extends javax.swing.JFrame {
 		layout.setHorizontalGroup(layout
 				.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
 				.addGroup(
-						javax.swing.GroupLayout.Alignment.TRAILING,
 						layout.createSequentialGroup()
-								.addContainerGap(
-										javax.swing.GroupLayout.DEFAULT_SIZE,
-										Short.MAX_VALUE)
+								.addContainerGap()
 								.addGroup(
 										layout.createParallelGroup(
-												javax.swing.GroupLayout.Alignment.LEADING,
-												false)
-												.addComponent(
-														jButton1,
-														javax.swing.GroupLayout.Alignment.TRAILING,
-														javax.swing.GroupLayout.DEFAULT_SIZE,
-														310, Short.MAX_VALUE)
+												javax.swing.GroupLayout.Alignment.LEADING)
 												.addComponent(jScrollPane1)
 												.addComponent(
 														jComboBox1,
@@ -120,8 +113,29 @@ public class GUI extends javax.swing.JFrame {
 																.addComponent(
 																		jComboBox5,
 																		0,
+																		61,
+																		Short.MAX_VALUE))
+												.addComponent(
+														jButton1,
+														javax.swing.GroupLayout.DEFAULT_SIZE,
+														javax.swing.GroupLayout.DEFAULT_SIZE,
+														Short.MAX_VALUE)
+												.addGroup(
+														layout.createSequentialGroup()
+																.addComponent(
+																		label1,
+																		javax.swing.GroupLayout.PREFERRED_SIZE,
 																		javax.swing.GroupLayout.DEFAULT_SIZE,
-																		Short.MAX_VALUE)))
+																		javax.swing.GroupLayout.PREFERRED_SIZE)
+																.addPreferredGap(
+																		javax.swing.LayoutStyle.ComponentPlacement.RELATED,
+																		javax.swing.GroupLayout.DEFAULT_SIZE,
+																		Short.MAX_VALUE)
+																.addComponent(
+																		jButton2,
+																		javax.swing.GroupLayout.PREFERRED_SIZE,
+																		103,
+																		javax.swing.GroupLayout.PREFERRED_SIZE)))
 								.addContainerGap()));
 		layout.setVerticalGroup(layout
 				.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -140,8 +154,7 @@ public class GUI extends javax.swing.JFrame {
 										javax.swing.GroupLayout.DEFAULT_SIZE,
 										javax.swing.GroupLayout.PREFERRED_SIZE)
 								.addPreferredGap(
-										javax.swing.LayoutStyle.ComponentPlacement.RELATED,
-										14, Short.MAX_VALUE)
+										javax.swing.LayoutStyle.ComponentPlacement.RELATED)
 								.addGroup(
 										layout.createParallelGroup(
 												javax.swing.GroupLayout.Alignment.BASELINE)
@@ -167,6 +180,22 @@ public class GUI extends javax.swing.JFrame {
 										23,
 										javax.swing.GroupLayout.PREFERRED_SIZE)
 								.addPreferredGap(
+										javax.swing.LayoutStyle.ComponentPlacement.RELATED,
+										javax.swing.GroupLayout.DEFAULT_SIZE,
+										Short.MAX_VALUE)
+								.addGroup(
+										layout.createParallelGroup(
+												javax.swing.GroupLayout.Alignment.LEADING)
+												.addComponent(
+														label1,
+														javax.swing.GroupLayout.Alignment.TRAILING,
+														javax.swing.GroupLayout.PREFERRED_SIZE,
+														javax.swing.GroupLayout.DEFAULT_SIZE,
+														javax.swing.GroupLayout.PREFERRED_SIZE)
+												.addComponent(
+														jButton2,
+														javax.swing.GroupLayout.Alignment.TRAILING))
+								.addPreferredGap(
 										javax.swing.LayoutStyle.ComponentPlacement.RELATED)
 								.addComponent(jScrollPane1,
 										javax.swing.GroupLayout.PREFERRED_SIZE,
@@ -178,14 +207,17 @@ public class GUI extends javax.swing.JFrame {
 	}// </editor-fold>
 
 	private void jList1ActionPerformed(java.awt.event.MouseEvent evt, int index) {
-		String routeName = listModel.get(index);
+		String routeName = (listModel.get(index).split(":"))[0];
 		try {
-			ResultSet rs = statement
+			pStatement = connection.prepareStatement("Select Count(*) from ROUTE_STOPS, ROUTE where Route_ID = RouteID AND RouteName = '" + routeName + "'");
+			ResultSet rs1 = pStatement.executeQuery();
+			ResultSet rs2 = statement
 					.executeQuery("Select StopName from BUS_STOP, ROUTE_STOPS, ROUTE WHERE StopID = Stop_ID AND Route_ID = RouteID AND RouteName = \""
 							+ routeName + "\"");
-			String routeStops = "Stops:\n";
-			while (rs.next()) {
-				routeStops = routeStops + (rs.getString("StopName")) + "\n";
+			rs1.next();
+			String routeStops = rs1.getInt(1)+ " Stops:\n";
+			while (rs2.next()) {
+				routeStops = routeStops + (rs2.getString("StopName")) + "\n";
 			}
 			JOptionPane.showMessageDialog(this.getContentPane(), routeStops,
 					"Showing info for " + routeName, JOptionPane.PLAIN_MESSAGE);
@@ -196,23 +228,32 @@ public class GUI extends javax.swing.JFrame {
 
 	private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
 		listModel.clear();
+	
 		if (((String) jComboBox1.getSelectedItem()).contains("Select")) {
 			JOptionPane.showMessageDialog(this.getContentPane(),
 					"Please select a starting route!", "Error",
 					JOptionPane.ERROR_MESSAGE);
 			return;
 		}
-
+		int day = jComboBox3.getSelectedIndex();
+		int time = jComboBox4.getSelectedIndex()
+				+ (jComboBox5.getSelectedIndex() * 12);
+		ResultSet rs;
 		if (((String) jComboBox2.getSelectedItem()).contains("Select")) {
-			ResultSet rs;
+
 			try {
-				System.out.println(jComboBox1.getSelectedItem());
+
 				rs = statement
-						.executeQuery("Select distinct RouteName from BUS_STOP, ROUTE_STOPS, ROUTE WHERE StopID = Stop_ID AND Route_ID = RouteID AND  StopName = \""
+						.executeQuery("Select distinct RouteName, Route_ID from BUS_STOP, ROUTE_STOPS, ROUTE, ROUTE_TIME WHERE StopID = Stop_ID AND Route_ID = RouteID AND Time_ID = TimeID AND "
+								+ day
+								+ " BETWEEN StartDay AND EndDay AND "
+								+ time
+								+ " BETWEEN StartTime and EndTime AND StopName = \""
 								+ jComboBox1.getSelectedItem() + "\"");
 
 				while (rs.next()) {
-					listModel.addElement(rs.getString("RouteName"));
+					listModel.addElement(rs.getString("RouteName") + ":       "
+							+ rs.getString("Route_ID"));
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
@@ -220,25 +261,33 @@ public class GUI extends javax.swing.JFrame {
 		}
 
 		else {
-			ResultSet rs;
+
 			try {
-				System.out.println(jComboBox1.getSelectedItem());
-				rs = statement
-						.executeQuery("Select distinct RouteName from BUS_STOP, ROUTE_STOPS, ROUTE WHERE StopID = Stop_ID AND Route_ID = RouteID AND  StopName = \""
+
+				rs= statement
+						.executeQuery("Select distinct RouteName, Route_ID from BUS_STOP, ROUTE_STOPS, ROUTE WHERE StopID = Stop_ID AND Route_ID = RouteID AND  StopName = \""
 								+ jComboBox1.getSelectedItem()
-								+ "\" AND RouteName IN (Select distinct RouteName from BUS_STOP, ROUTE_STOPS, ROUTE WHERE StopID = Stop_ID AND Route_ID = RouteID AND  StopName = \""
+								+ "\" AND RouteName IN (Select distinct RouteName from BUS_STOP, ROUTE_STOPS, ROUTE, ROUTE_TIME WHERE StopID = Stop_ID AND Route_ID = RouteID AND Time_ID = TimeID AND "
+								+ day
+								+ " BETWEEN StartDay AND EndDay AND "
+								+ time
+								+ " BETWEEN StartTime and EndTime AND StopName = \""
 								+ jComboBox2.getSelectedItem() + "\")");
+				 
+						 
 
 				while (rs.next()) {
-					listModel.addElement(rs.getString("RouteName"));
+					listModel.addElement(rs.getString("RouteName") + ":       "
+							+ rs.getString("Route_ID"));
 				}
+				label1.setText("kekkerz");
+				label1.revalidate();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
+
 				e.printStackTrace();
 			}
 		}
 	}
-
 
 	public static void main(String args[]) {
 		stops = new ArrayList<String>();
@@ -252,7 +301,7 @@ public class GUI extends javax.swing.JFrame {
 					.getConnection("jdbc:sqlite:src/dbproject/bcrta.db");
 			statement = connection.createStatement();
 			statement.setQueryTimeout(30);
-			ResultSet rs = statement.executeQuery("Select * FROM Bus_Stop");
+			ResultSet rs = statement.executeQuery("Select StopName FROM Bus_Stop");
 			stops.add("Select starting route");
 			while (rs.next()) {
 				stops.add(rs.getString("StopName"));
@@ -296,19 +345,23 @@ public class GUI extends javax.swing.JFrame {
 		});
 	}
 
-	private String[] days = {"Sunday", "Monday", "Tuesday",
-			"Wednesday", "Thursday", "Friday", "Saturday" };
-	private String[] times = { "12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11" };
+	private String[] days = { "Sunday", "Monday", "Tuesday", "Wednesday",
+			"Thursday", "Friday", "Saturday" };
+	private String[] times = { "12", "1", "2", "3", "4", "5", "6", "7", "8",
+			"9", "10", "11" };
 	private static DefaultListModel<String> listModel;
 	private static ArrayList<String> stops;
 	private static Statement statement;
+	private static PreparedStatement pStatement;
 	private static Connection connection;
 	private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
 	private javax.swing.JComboBox<String> jComboBox1;
 	private javax.swing.JComboBox<String> jComboBox2;
-	private javax.swing.JComboBox jComboBox3;
-	private javax.swing.JComboBox jComboBox4;
-	private javax.swing.JComboBox jComboBox5;
+	private javax.swing.JComboBox<String> jComboBox3;
+	private javax.swing.JComboBox<String> jComboBox4;
+	private javax.swing.JComboBox<String> jComboBox5;
+	private java.awt.Label label1;
 	private javax.swing.JList<String> jList1;
 	private javax.swing.JScrollPane jScrollPane1;
 
